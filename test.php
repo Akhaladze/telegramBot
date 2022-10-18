241c741a83c4db23852ef405d7cde2b3ba8f63b8
root <root@dev.akademorto.kz>
2022-10-18 21:33:52 +0600

    Delete tmp files

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "Test completed";
-$latestupdateid = "123423423\n";
-$myfile = fopen("/var/www.api/telegramBot/testfile.txt", "w");
-fwrite($myfile, $latestupdateid);
-fclose($myfile);
-?>
